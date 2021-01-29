e8efb7f839cd318b7a9a977505ec11ee8cfb3ff9
adamrturman <adamrturman@gmail.com>
2021-01-29 15:53:10 -0600

    rendering totals by year after creating a map to accumulate the data, then coverting to an array to render in the DOM

--- src/App.js
+++ src/App.js
@@ -1,91 +1,106 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([])
-  const [customers, setCustomers] = useState([])
-  const [showData, setShowData] = useState(false)
+  const [data, setData] = useState([]);
+  const [customers, setCustomers] = useState([]);
+  const [showData, setShowData] = useState(false);
 
-  const displayData = (event) => {
-    fetch("/2020/orders-2020-02-10.json")
-      .then((response) => response.json())
-      .then((data) => setData(data.orders), setCustomers(data.customers))
-      .then(setShowData(true))
+  const fetchData = () => {
+    axios.get("/2020/orders-2020-02-10.json")
+      .then(({ data }) => {
+        setData(data.orders);
+        setCustomers(data.customers);
+      })
+      .then(setShowData(true));
   }
-
-  console.log("here are customers", customers)
-
-  const individualCustomers = data.map((customer) => {
-    return customer.items.map((item) => (
-      <li>{item.name} - {item.price}</li>
-    ))
-  })
 
   const totalPrices = data.map((customer) => {
-      return parseInt(customer.total_price)
-    }
+    return parseInt(customer.total_price);
+  }
   )
 
-  const maxTotalPrice = Math.max(...totalPrices)
+  const maxTotalPrice = Math.max(...totalPrices);
 
-//  2. calculate and print the sum of prices for all
-//   orders created in the previous three years, grouped by year.
-//  Total price of orders in 2018 = 275.00
-//   Total price of orders in 2019 = 860.00
-//   Total price of orders in 2020 =  20.00
+  // as time allows
+  // 0. service(s), no logic in App.js, separate data vs. presentational components
+  // 1. TypeScript
+  // 2. Linting
 
-    //  Loop through the `orders` and check the first four characters of the `created_date` property
-    //  Push the `total_price` property of that order into an array for that specific year
-    //  After the loop is completed, reduce those year arrays into a single sum and render in the DOM
-    //  
+  //  2. calculate and print the sum of prices for all
+  //   orders created in the previous three years, grouped by year.
+  //  Total price of orders in 2018 = 275.00
+  //   Total price of orders in 2019 = 860.00
+  //   Total price of orders in 2020 =  20.00
+
+  //  Loop through the `orders` and check the first four characters of the `created_date` property --edit: loop through years
+  //  Push the `total_price` property of that order into an array for that specific year
+  //  After the loop is completed, reduce those year arrays into a single sum and render in the DOM
+  //  
+
+  function printTotalPriceOfOrders () {
+    const sumByYear = data.reduce((accumulator, order) => {
+      const orderYear = new Date(order.created_date).getFullYear();
+      if (accumulator.has(orderYear)) {
+        const currentSumByYear = accumulator.get(orderYear);
+        accumulator.set(orderYear, currentSumByYear + order.total_price);
+      } else {
+        accumulator.set(orderYear, order.total_price)
+      }
+      return accumulator;
+    }, new Map());
+    return sumByYear;
+  }
+
+  const totalPriceOfOrders = printTotalPriceOfOrders()
+
+  
+    const arrayOfTotalsByYear = Array.from(totalPriceOfOrders, ([year, total]) => ({ year, total }));
+    // console.log("arrayOfTotalsByYear", arrayOfTotalsByYear)
+
+    const mappedTotalsByYear = arrayOfTotalsByYear.map((year) => (
+      <li>{year.year} - {year.total}</li>
+    ))
 
   //  3. Find the maxCustomer by finding the order whose `total_price` matches `maxTotalPrice`
-  const maxOrder = data.filter(order => order.total_price === maxTotalPrice) 
-  const maxOrderObject = maxOrder[0]
+  const maxOrder = data.filter(order => order.total_price === maxTotalPrice)
+
   //  Then grab that `customer_id` and find the `customer` who matches that and return their `name`
-  function findMaxOrderId (maxOrder) {
-    if(maxOrderObject) {
-      return maxOrderObject.customer_id
-    }
-  }
-  const maxOrderId = findMaxOrderId(maxOrderObject)
-
-  function findCustomerWithMaxOrder (maxOrderId, customers) {
-    console.log("customers in foundCustomer", customers)
-    if (customers) {
-      console.log("in foundCustomer function")
-      const foundCustomer = customers.filter(customer => customer.id === maxOrderId)
-      return foundCustomer.name
+  function findMaxOrderCustomerId(maxOrder) {
+    if (maxOrder) {
+      return maxOrder.customer_id
     }
   }
 
-  console.log(typeof maxOrder)
-  console.log("maxOrder", maxOrder)
-  console.log("maxOrderObject", maxOrderObject)
-  console.log("Customer with max order", maxOrderId)
-  console.log("Name", findCustomerWithMaxOrder(maxOrderId, data.customers))
+  const maxOrderCustomerId = findMaxOrderCustomerId(maxOrder[0])
 
+  function findCustomerWithMaxOrder(maxOrderId, customers) {
+    if (customers.length > 0) {
+      const foundCustomer = customers.find(customer => customer.id === maxOrderId);
+      return foundCustomer.name;
+    }
+  }
 
-  // const individualCustomers = data.map((customer) => {
-  //   return customer.items.filter((item) => (
-  //     Math.max(item.price)))
-  // })
+  console.log("Customer with max order", maxOrderCustomerId)
+  console.log("Name", findCustomerWithMaxOrder(maxOrderCustomerId, customers))
 
   const mappedCustomers = data.map((order) => {
     return <li>{order.customer_id}</li>
-})
- 
+  });
+
   return (
     <div className="App">
       <h1>Research Square</h1>
-      <button onClick={displayData}>Click</button>
+      <button onClick={fetchData}>Click</button>
       {showData ?
-      <>
-      <h2>Most expensive order = {maxTotalPrice}</h2>
-      <h2>Customer with the most orders = </h2>
-      <div>{mappedCustomers} - {individualCustomers}</div>
-      </>
-      : null}
+        <>
+          <h2>Most expensive order = {maxTotalPrice}</h2>
+          <h2>Customer with the most orders = </h2>
+          <h2>Total Orders by Year: {mappedTotalsByYear}</h2>
+          <div>{mappedCustomers}</div>
+        </>
+        : null}
     </div>
   );
 }
