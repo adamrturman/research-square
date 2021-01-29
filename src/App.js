21a5e004036641717b670a29ff6f23cfa53a5729
adamrturman <adamrturman@gmail.com>
2021-01-29 13:28:51 -0600

    initial commit - set up proxy server and rendering data on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+  const [showData, setShowData] = useState(false)
+
+  const displayData = (event) => {
+    fetch("/2020/orders-2020-02-10.json")
+      .then((response) => response.json())
+      .then((data) => setData(data.orders))
+      .then(setShowData(true))
+  }
+
+  const individualCustomers = data.map((customer) => {
+    return customer.items.map((item) => (
+      <li>{item.name} - {item.price}</li>
+    ))
+  })
+
+  console.log("individualCustomers", individualCustomers)
+
+  const mappedCustomers = data.map((order) => {
+    return <li>{order.customer_id}</li>
+})
+
+  // const mappedItems = individualCustomers.map((item) => (
+  //   <li>{item.name} - {item.price}</li>
+  // ))
+
+  console.log("mappedCustomers", mappedCustomers)
+
+  // console.log("mappedItems", mappedItems)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Research Square</h1>
+      <button onClick={displayData}>Click</button>
+      <div>{mappedCustomers} - {individualCustomers}</div>
     </div>
   );
 }
